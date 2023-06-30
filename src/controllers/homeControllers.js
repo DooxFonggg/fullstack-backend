ff0022c31a3eaec6b4bbfe70daaa37fbbca06915
DooxFonggg <phongcoi1235@gmail.com>
2023-06-30 23:50:18 +0700

    buoi 33

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,5 +1,17 @@
+const connection = require('../config/database');
+
+
 const getHomepage = (req, res) => {
-    res.send('Hello World! toi ten la fonggg & nodemon')
+    let users = [];
+    connection.query(
+        'SELECT *FROM Users u ',
+        function (err, results, fields) {
+            users = results;
+            console.log('>> results', results); // results contains rows returned by server    
+            //console.log(">> user", users);
+            res.send(JSON.stringify(users));
+        }
+    );
 }
 
 const getABC = (req, res) => {
