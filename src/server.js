72511c1002473f711fa06b00c17e0a7bec237bf5
DooxFonggg <phongcoi1235@gmail.com>
2023-06-30 18:52:33 +0700

    buoi 30

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const express = require('express'); // commonjs  khai bao thu vien
 const configviewEngine = require('./config/viewEngine');
 const webRoute = require('./routes/web');
 //console.log(">>> check env: ", process.env);
+// get the client
+const mysql = require('mysql2')
 
 
 
@@ -16,6 +18,23 @@ configviewEngine(app);
 
 //khai bao route
 app.use('/', webRoute);
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'hoidanit',
+    port: 3307,
+    password: '123456'
+});
+// simple query
+connection.query(
+    'SELECT *FROM Users u ',
+    function (err, results, fields) {
+        console.log('>> results', results); // results contains rows returned by server
+        console.log('>> fields', fields); // fields contains extra meta data about results, if available
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
