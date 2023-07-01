db5854896df4f8d90e1e983a272a526ae06e4d28
DooxFonggg <phongcoi1235@gmail.com>
2023-07-01 09:57:30 +0700

    buoi 35

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -2,16 +2,18 @@ const connection = require('../config/database');
 
 
 const getHomepage = (req, res) => {
-    let users = [];
-    connection.query(
-        'SELECT *FROM Users u ',
-        function (err, results, fields) {
-            users = results;
-            console.log('>> results', results); // results contains rows returned by server    
-            //console.log(">> user", users);
-            res.send(JSON.stringify(users));
-        }
-    );
+
+    res.render('home.ejs');
+    // let users = [];
+    // connection.query(
+    //     'SELECT *FROM Users u ',
+    //     function (err, results, fields) {
+    //         users = results;
+    //         console.log('>> results', results); // results contains rows returned by server    
+    //         //console.log(">> user", users);
+    //         res.send(JSON.stringify(users));
+    //     }
+    // );
 }
 
 const getABC = (req, res) => {
