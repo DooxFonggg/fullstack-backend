ae40eea53dfd34936e7f8d2e1c2aee28f971723e
DooxFonggg <phongcoi1235@gmail.com>
2023-08-01 22:54:24 +0700

    buoi 37

--- src/config/database.js
+++ src/config/database.js
@@ -4,12 +4,15 @@ const mysql = require('mysql2');
 
 
 // create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 module.exports = connection;
