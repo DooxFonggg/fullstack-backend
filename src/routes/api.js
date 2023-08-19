e24066de3515195d0fe70d037d0be380e0ae14a9
DooxFonggg <phongcoi1235@gmail.com>
2023-08-19 15:40:32 +0700

    buoi 74

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const routerAPI = express.Router();
-const { getUsersAPI } = require('../controllers/apiControllers');
+const { getUsersAPI, postCreateUserAPI } = require('../controllers/apiControllers');
 
 // khai bao roude 
 routerAPI.get('/users', getUsersAPI);
-
+routerAPI.post('/users', postCreateUserAPI);
 //export qua server
 module.exports = routerAPI;
