15afc4411ca9630a91f44c22c9fc3b32c0e02a3b
DooxFonggg <phongcoi1235@gmail.com>
2023-08-19 16:29:08 +0700

    buoi 76

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -21,8 +21,19 @@ const postCreateUserAPI = async (req, res) => {
         errorCode: 0,
         data: user
     });
-
+}
+const postUpdateUserAPI = async (req, res) => {
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let id = req.body.id;
+    // await updateByID(email, name, city, id);
+    let user = await User.findOneAndUpdate({ _id: id }, { email: email, name: name, city: city });
+    res.status(200).json({
+        errorCode: 0,
+        data: user
+    });
 }
 module.exports = {
-    getUsersAPI, postCreateUserAPI
+    getUsersAPI, postCreateUserAPI, postUpdateUserAPI
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routerAPI = express.Router();
-const { getUsersAPI, postCreateUserAPI } = require('../controllers/apiControllers');
+const { getUsersAPI, postCreateUserAPI, postUpdateUserAPI } = require('../controllers/apiControllers');
 
 // khai bao roude 
 routerAPI.get('/users', getUsersAPI);
 routerAPI.post('/users', postCreateUserAPI);
+routerAPI.put('/users', postUpdateUserAPI);
 //export qua server
 module.exports = routerAPI;
