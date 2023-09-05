814e93af9c8500815fe4176fe4f0a862255ef3d6
DooxFonggg <phongcoi1235@gmail.com>
2023-09-05 12:51:50 +0700

    buoi 116

--- src/services/projectServices.js
+++ src/services/projectServices.js
@@ -2,8 +2,30 @@ const project = require('../models/project');
 
 const createProjectService = async (arr) => {
     try {
-        let result = await project.insertMany(arr);
-        console.log('>> check result:', result);
+        if (arr.type === "EMPTY-PROJECT") {
+            let result = await project.insertMany(arr);
+            console.log('>> check result:', result);
+        }
+        // them user vao du an
+        if (arr.type === "ADD-USER") {
+            console.log('>> check data  ', arr);
+            // lay project
+            let myProject = await project.findById(arr.projectId).exec();// tim project bang id
+
+
+            // duyet qua arr cua phan nhap posman
+            // Kiểm tra trùng lặp trong mảng usersInfor
+            for (let i = 0; i < arr.userId.length; i++) {
+                const userIdToAdd = arr.userId[i];
+                // Kiểm tra xem userIdToAdd đã tồn tại trong mảng usersInfor chưa
+                if (!myProject.usersInfor.includes(userIdToAdd)) {
+                    myProject.usersInfor.push(userIdToAdd);
+                }
+            }
+            let newResult = await myProject.save();
+            return newResult;
+        }
+
         return result;
     } catch (error) {
         console.log('>> check err', error);
